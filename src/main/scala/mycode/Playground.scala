0e488c14e00562899a388e0d3bdc416374db618f
Daniel <daniel.aronovich@dualbird.io>
2022-11-29 17:59:40 +0200

    Made minor structure changes

--- src/main/scala/mycode/Playground.scala
+++ src/main/scala/mycode/Playground.scala
@@ -9,27 +9,8 @@ package mycode
   * Daniel @ Rock the JVM
   */
 object Playground extends App {
-  // calculate abs function
-  def abs(x: Double) = if (x >= 0) x else -x
 
 
-  // calculate square root function
-
-  def sqrt(x: Double): Double = {
-
-    def sqrtIter(guess: Double, x: Double): Double =
-      if (isGoodEnough(guess, x)) guess
-      else sqrtIter(improve(guess, x), x)
-
-    def improve(guess: Double, x: Double): Double =
-      (guess + x / guess) / 2
-
-    def isGoodEnough(guess: Double, x: Double): Boolean =
-      abs(guess * guess - x) < 0.001
-
-    sqrtIter(1.0, x)
-  }
-
-  println(sqrt(0.1e-20))
+  println("This will be used in the future")
 
 }
